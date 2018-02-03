27a6de5d8ab03cbf6dac8d3adf46ad04ab10373b
dskrypa <dskrypa@gmail.com>
2018-02-03 17:06:27 -0500

    cleaned up the old files a bit

--- bogobogosort.py
+++ bogobogosort.py
@@ -1,115 +1,120 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 '''
 Bogobogosort
-Author: Douglas Skrypa
-Version: 2016.02.05
 
 This implementation of Bogobogosort is based on the description found here:
 http://www.dangermouse.net/esoteric/bogobogosort.html
 '''
 
-import random, time, sys;
-from argparse import ArgumentParser;
+import random
+import sys
+import time
+from argparse import ArgumentParser
+
 
 def main():
-	parser = ArgumentParser(description="Bogobogosort");
-	parser.add_argument("--value", "-v", type=int, nargs="+", help="Values to sort", action="append");
-	parser.add_argument("--nargs", "-n", type=int, help="Number of values to generate to sort");
-	parser.add_argument("--test", "-t", type=int, metavar="maxN", help="Test with list lengths up to maxN");
-	parser.add_argument("--count", "-c", type=int, help="Number of trials to test. Only applies when running a test.", default=5);
-	args = parser.parse_args();
-	
-	if (args.value == None) and (args.nargs == None) and (args.test == None):
-		parser.print_help();
-		parser.exit(0, "At least one argument is required.\n");
-	elif (args.test != None) and ((args.value != None) or (args.nargs != None)):
-		parser.print_help();
-		parser.exit(0, "Invalid combination of arguments.\n");
-	elif (args.value != None) and (args.nargs != None):
-		parser.print_help();
-		parser.exit(0, "Provide a list of numbers, or a number of values to generate, not both.\n");
-		
-	if (args.test != None):
-		bbsTest(args.test+1, args.count);
-	else:
-		if (args.value != None):
-			vals = [item for sublist in args.value for item in sublist];
-		else:
-			vals = randlist(0, 10000, args.nargs);
-		
-		print("Sorting {}...".format(vals));
-		pt = PerfTimer();
-		srtd = bogobogosort(vals);
-		elapsed = pt.elapsedf();
-		print("Sorted {} in {}".format(srtd, elapsed));
-#/main
+    parser = ArgumentParser(description="Bogobogosort")
+    parser.add_argument("--value", "-v", type=int, nargs="+", help="Values to sort", action="append")
+    parser.add_argument("--nargs", "-n", type=int, help="Number of values to generate to sort")
+    parser.add_argument("--test", "-t", type=int, metavar="maxN", help="Test with list lengths up to maxN")
+    parser.add_argument("--count", "-c", type=int, help="Number of trials to test. Only applies when running a test.", default=5)
+    args = parser.parse_args()
+
+    if (args.value is None) and (args.nargs is None) and (args.test is None):
+        parser.print_help()
+        parser.exit(0, "value, nargs, or test is required.\n")
+    elif (args.test is not None) and ((args.value is not None) or (args.nargs is not None)):
+        parser.print_help()
+        parser.exit(0, "Invalid combination of arguments.\n")
+    elif (args.value is not None) and (args.nargs is not None):
+        parser.print_help()
+        parser.exit(0, "Provide a list of numbers, or a number of values to generate, not both.\n")
+
+    if args.test is not None:
+        bbsTest(args.test+1, args.count)
+    else:
+        if args.value is not None:
+            vals = [item for sublist in args.value for item in sublist]
+        else:
+            vals = randlist(0, 10000, args.nargs)
+
+        print("Sorting {}...".format(vals))
+        pt = PerfTimer()
+        srtd = bogobogosort(vals)
+        elapsed = pt.elapsedf()
+        print("Sorted {} in {}".format(srtd, elapsed))
+
 
 def randlist(minval, maxval, count):
-	return [random.randint(minval, maxval) for c in range(count)];
-#/randlist
+    return [random.randint(minval, maxval) for c in range(count)]
+
 
 def bbsTest(maxN, trials):
-	fmt = "{:11.4f}s for N={}: {} -> {}";
-	tests = {};
-	for n in range(1, maxN):
-		tests[n] = [];
-		for t in range(trials):
-			vals = randlist(0, 10000, n);
-			pt = PerfTimer();
-			srtd = bogobogosort(vals);
-			elapsed = pt.elapsed();
-			tests[n].append(elapsed);
-			print(fmt.format(elapsed, n, vals, srtd));
-	
-	thdr = "  N  | Average Time";
-	tfmt = "{:4d} | {:11.4f}s";
-	print(thdr);
-	print("-" * len(thdr));
-	for n in range(1, maxN):
-		ttime = sum(tests[n]);
-		atime = ttime / trials;
-		print(tfmt.format(n, atime));
-#/bbsTest
+    fmt = "{:11.4f}s for N={}: {} -> {}"
+    tests = {}
+    for n in range(1, maxN):
+        tests[n] = []
+        for t in range(trials):
+            vals = randlist(0, 10000, n)
+            pt = PerfTimer()
+            srtd = bogobogosort(vals)
+            elapsed = pt.elapsed()
+            tests[n].append(elapsed)
+            print(fmt.format(elapsed, n, vals, srtd))
+
+    thdr = "  N  | Average Time"
+    tfmt = "{:4d} | {:11.4f}s"
+    print(thdr)
+    print("-" * len(thdr))
+    for n in range(1, maxN):
+        ttime = sum(tests[n])
+        atime = ttime / trials
+        print(tfmt.format(n, atime))
+
 
 def bogobogosort(arr):
-	cpy = list(arr);						#Create a copy of the list
-	last = len(cpy) - 1;					#Determine the index of the last element in the list
-	if (last < 1):							#If the last index is less than 1 (i.e., there are fewer than 2 elements)
-		return cpy;							#Then the list is sorted
-	subl = bogobogosort(cpy[:last]);		#Sort the first n-1 elements of the list
-	if (cpy[last] >= max(subl)):			#If the nth element is greater than or equal to the max of the first n-1 elements
-		srtd = subl + [cpy[last]];			#Then the list is sorted
-		sameAsOrig = (srtd == arr);			#Check to see if the copy is in the same order as the original list
-		return srtd;						#Return the sorted list
-	else:									#If the nth element doesn't have the highest value
-		random.shuffle(cpy);				#Randomize the order of the elements in the list
-		return bogobogosort(cpy);			#Run bogobogosort on the shuffled copy
-#/bogobogosort
+    cpy = list(arr)						#Create a copy of the list
+    last = len(cpy) - 1					#Determine the index of the last element in the list
+    if (last < 1):						#If the last index is less than 1 (i.e., there are fewer than 2 elements)
+        return cpy						#Then the list is sorted
+    subl = bogobogosort(cpy[:last])		#Sort the first n-1 elements of the list
+    if (cpy[last] >= max(subl)):		#If the nth element is greater than or equal to the max of the first n-1 elements
+        srtd = subl + [cpy[last]]		#Then the list is sorted
+        sameAsOrig = (srtd == arr)		#Check to see if the copy is in the same order as the original list
+        return srtd						#Return the sorted list
+    else:								#If the nth element doesn't have the highest value
+        random.shuffle(cpy)				#Randomize the order of the elements in the list
+        return bogobogosort(cpy)		#Run bogobogosort on the shuffled copy
+
 
 def fTime(seconds):
-	orig = seconds;
-	s = int(seconds);
-	rmd = orig - s;
-	m, s = divmod(s, 60);
-	h, m = divmod(m, 60);
-	return "{:02d}:{:02d}:{:07.4f}".format(h, m, s + rmd);						#Return a string representation of the given number of seconds as HH:MM:SS.ssss
-#/fTime
+    """Returns a string representation of the given number of seconds as HH:MM:SS.ssss"""
+    orig = seconds
+    s = int(seconds)
+    rmd = orig - s
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+    return "{:02d}:{:02d}:{:07.4f}".format(h, m, s + rmd)
+
 
 class PerfTimer():
-	def __init__(self):
-		PY2 = (sys.version_info.major == 2);									#Determine whether or not the current version of Python is 2.x
-		self.now = time.time if PY2 else time.perf_counter;						#Pick the timer function based on the current version of Python
-		self.start = self.now();												#Initialize the timer with the current time
-	def time(self):
-		return self.now();														#Return the current time using the same method as the internal timer
-	def elapsed(self, since=None):
-		sinceTime = self.start if (since == None) else since;					#Calculate time since the given time, or since initialization
-		return self.now() - sinceTime;											#Return the time delta in seconds
-	def elapsedf(self, since=None):
-		return fTime(self.elapsed(since));										#Return the time delta as a string in the form HH:MM:SS
-#/PerfTimer
+    def __init__(self):
+        PY2 = (sys.version_info.major == 2)
+        self.now = time.time if PY2 else time.perf_counter
+        self.start = self.now()
+
+    def time(self):
+        return self.now()
+
+    def elapsed(self, since=None):
+        """Calculates the time since the given time or since initialization"""
+        sinceTime = self.start if since is None else since
+        return self.now() - sinceTime
+
+    def elapsedf(self, since=None):
+        """Returns the time delta as a string in the form HH:MM:SS"""
+        return fTime(self.elapsed(since))
+
 
 if __name__ == "__main__":
-	main();
-#/__main__
+    main()
--- common.py
+++ common.py
@@ -1,119 +1,116 @@
 # -*- coding: UTF-8 -*-
+"""
+Old library of functions that I used often while learning...  Cleaned up slightly.
+"""
 
-'''
-Author: Douglas Skrypa
-Date: 2016.01.23
-Version: 1.3
-'''
+import os
+import re
+import sys
+import time
 
-import os, sys, time, re;
 
 def getPaths(path):
-	'''
-	Recursively generates a list of absolute paths for every file discoverable
-	via the given path.
-	'''
-	path = path[:-1] if (path[-1:] == "/") else path;							#Strip trailing slash if it exists
-	paths = [];																	#Initialize list to store paths in
-	if os.path.isdir(path):														#If the given path is a directory
-		for sub in os.listdir(path):											#Iterate through each sub-path in it
-			paths += getPaths(path + "/" + sub);								#Add the list of paths discoverable there
-	elif os.path.isfile(path):													#Otherwise, if it is a file
-		paths += [path];														#Add the path to the list
-	return paths;																#Return the list
-#/getPaths
+    """Recursively generates a list of absolute paths for every file discoverable via the given path."""
+    path = path[:-1] if (path[-1:] == "/") else path
+    paths = []
+    if os.path.isdir(path):
+        for sub in os.listdir(path):
+            paths += getPaths(path + "/" + sub)
+    elif os.path.isfile(path):
+        paths += [path]
+    return paths
+
 
 def getFilteredPaths(path, ext, sort=True):
-	paths = getPaths(path);														#Get the paths
-	fileFilter = re.compile(r'.*\.' + ext, re.IGNORECASE);						#Define the filter
-	filtered = [fname for fname in paths if fileFilter.match(fname)];			#Apply the filter
-	return sorted(filtered) if sort else filtered;								#Return the filtered list (sorted if sort == True)
-#/getFilteredPaths
+    paths = getPaths(path)
+    fileFilter = re.compile(r'.*\.' + ext, re.IGNORECASE)
+    filtered = [fname for fname in paths if fileFilter.match(fname)]
+    return sorted(filtered) if sort else filtered
+
 
 def fTime(seconds):
-	seconds = int(seconds);
-	minutes = int(seconds / 60);
-	seconds -= (minutes * 60);
-	hours = int(minutes / 60);
-	minutes -= (hours * 60);
-	return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds);
-#/fTime
+    seconds = int(seconds)
+    minutes = int(seconds / 60)
+    seconds -= (minutes * 60)
+    hours = int(minutes / 60)
+    minutes -= (hours * 60)
+    return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
 
 class PerfTimer():
-	'''Simple performance monitor including a timer and counters'''
-	def __init__(self):
-		self.start = time.perf_counter();										#Initialize the timer with the current time
-	#/init
-	def time(self):
-		return time.perf_counter();												#Return the current time using the same method as the internal timer
-	#/time	
-	def elapsed(self, since=None):
-		sinceTime = self.start if (since == None) else since;
-		return time.perf_counter() - sinceTime;									#Return the time delta in seconds since initialization
-	#/elapsed
-	def elapsedf(self):
-		return time.strftime("%H:%M:%S",time.gmtime(self.elapsed()));			#Return the time delta as a string in the form HH:MM:SS
-	#/elapsedf
-#/PerfTimer
+    """Simple performance monitor including a timer and counters"""
+    def __init__(self):
+        self.start = time.perf_counter()
+
+    def time(self):
+        """Return the current time using the same method as the internal timer"""
+        return time.perf_counter()
+
+    def elapsed(self, since=None):
+        """Return the time delta in seconds since initialization"""
+        sinceTime = self.start if since is None else since
+        return time.perf_counter() - sinceTime
+
+    def elapsedf(self):
+        """Return the time delta as a string in the form HH:MM:SS"""
+        return time.strftime("%H:%M:%S",time.gmtime(self.elapsed()))
+
 
 class clio():
-	'''Command Line Interface Output'''
-	lml=0;
-	#utfout = open(1, 'w', encoding="utf-8", closefd=False);
-	@classmethod
-	def _fmt(cls, msg):
-		'''Format the given message for overwriting'''
-		mlen = len(msg);
-		suffix = " " * (clio.lml-mlen) if (mlen < clio.lml) else "";
-		clio.lml = mlen;
-		return "\r" + msg + suffix;
-	#/fmt
-	@classmethod
-	def show(cls, msg=""):
-		'''Display overwritable message'''
-		wmsg = cls._fmt(msg);	#.encode("utf-8");
-		#clio.utfout.write(wmsg);
-		#clio.utfout.flush();
-		sys.stdout.write(wmsg);
-		sys.stdout.flush();
-	#/show
-	@classmethod
-	def showf(cls, fmt, *args):
-		'''Display formatted overwritable message'''
-		msg = fmt.format(*args);
-		cls.show(msg);
-	#/showf
-	@classmethod
-	def println(cls, msg=""):
-		'''Display message on a new line'''
-		wmsg = cls._fmt(msg + "\n");	#.encode("utf-8");
-		#sys.stdout.write(cls._fmt(msg) + "\n");
-		#clio.utfout.write(wmsg);
-		#clio.utfout.flush();
-		sys.stdout.write(wmsg);
-		sys.stdout.flush();
-	#/println
-	@classmethod
-	def printf(cls, fmt, *args):
-		'''Display formatted message on a new line'''
-		msg = fmt.format(*args);
-		cls.println(msg);
-	#/printf
-#/clio
+    """Command Line Interface Output"""
+    lml=0;
+    #utfout = open(1, 'w', encoding="utf-8", closefd=False)
+    @classmethod
+    def _fmt(cls, msg):
+        """Format the given message for overwriting"""
+        mlen = len(msg)
+        suffix = " " * (clio.lml-mlen) if mlen < clio.lml else ""
+        clio.lml = mlen
+        return "\r" + msg + suffix
+
+    @classmethod
+    def show(cls, msg=""):
+        """Display overwritable message"""
+        wmsg = cls._fmt(msg)	#.encode("utf-8")
+        #clio.utfout.write(wmsg)
+        #clio.utfout.flush()
+        sys.stdout.write(wmsg)
+        sys.stdout.flush()
+
+    @classmethod
+    def showf(cls, fmt, *args):
+        """Display formatted overwritable message"""
+        msg = fmt.format(*args)
+        cls.show(msg)
+
+    @classmethod
+    def println(cls, msg=""):
+        """Display message on a new line"""
+        wmsg = cls._fmt(msg + "\n")	#.encode("utf-8")
+        #sys.stdout.write(cls._fmt(msg) + "\n")
+        #clio.utfout.write(wmsg)
+        #clio.utfout.flush()
+        sys.stdout.write(wmsg)
+        sys.stdout.flush()
+
+    @classmethod
+    def printf(cls, fmt, *args):
+        """Display formatted message on a new line"""
+        msg = fmt.format(*args)
+        cls.println(msg)
+
 
 class ErrorLog():
-	'''Simple error log that includes a date+time stamp for each line'''
-	def __init__(self, path):
-		self.logfile = open(path, "a", encoding="utf-8");
-		self.tfmt = "%Y-%m-%d_%H:%M:%S";
-		self.lfmt = "[{}] {}\n";
-	#/init
-	def record(self, err):
-		ts = time.strftime(self.tfmt, time.localtime());
-		self.logfile.write(self.lfmt.format(ts, err));
-		self.logfile.flush();
-	#/record
-	def close(self):
-		self.logfile.close();
-	#/close
-#/ErrorLog
+    """Simple error log that includes a date+time stamp for each line"""
+    def __init__(self, path):
+        self.logfile = open(path, "a", encoding="utf-8")
+        self.tfmt = "%Y-%m-%d_%H:%M:%S"
+        self.lfmt = "[{}] {}\n"
+
+    def record(self, err):
+        ts = time.strftime(self.tfmt, time.localtime())
+        self.logfile.write(self.lfmt.format(ts, err))
+        self.logfile.flush()
+
+    def close(self):
+        self.logfile.close()
